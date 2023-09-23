d49cd79da4de0e73c39d5d439a22d2f28f3f491d
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-09-24 02:26:40 +0300

    v1.3.1

--- src/components/Calendar/CalendarDay.ts
+++ src/components/Calendar/CalendarDay.ts
@@ -1,40 +1,18 @@
 export default function renderDayBtn(month: string, day: number): string {
   return `
   <!-- ${month}-[${day}] -->
-      <div class="w-20 h-20 flex justify-center items-end relative">
-              <div
-                class="absolute top-0.5 bg-gold2 w-3 h-3 rounded-full left-4 opacity-10"></div>
-              <div
-                class="absolute top-0.5 bg-gold2 w-3 h-3 rounded-full right-4 opacity-10"></div>
-              <div
-                class="absolute top-5 bg-gold2 w-2 h-2 rounded-full right-1 opacity-10"></div>
-              <div
-                class="absolute top-5 bg-gold2 w-2 h-2 rounded-full left-1 opacity-10"></div>
-              <div
-                class="absolute bottom-1 bg-gold2 w-2 h-2 rounded-full right-1 opacity-10"></div>
-              <div
-                class="absolute bottom-1 bg-gold2 w-2 h-2 rounded-full left-1 opacity-10"></div>
-              <button id="${month}-${day}"
-                class="bg-gold w-14 h-14 hex flex justify-center items-center opacity-50 hover:opacity-100 transition duration-300 grayscale-[35%] hover:grayscale-0">
-                <div
-                  class="hex bg-black w-[3.25rem] h-[3.25rem] flex justify-center items-center">
-                  <div
-                    class="hex bg-gold  w-12 h-12 flex justify-center items-center">
-                    <div
-                      class="hex bg-black w-11 h-11 flex justify-center items-center">
-                      <div
-                        class="hex bg-gold w-10 h-10 flex justify-center items-center pb-1">
-                        <span
-                          class="text-white font-semibold text-center text-xl transition duration-300">
-                          ${day}
-                        </span>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </button>
-            </div>
-          </div>
-<!-- -->
-    `;
+<div class="flex justify-center items-end relative my-4 ">
+    <button id="${month}-${day}" class="bg-gold w-14 h-14 hex flex justify-center items-center opacity-50 hover:opacity-100 transition duration-300 grayscale-[35%] hover:grayscale-0">
+  <div class="hex bg-black w-[3.25rem] h-[3.25rem] flex justify-center items-center">
+  <div class="hex bg-gold  w-12 h-12 flex justify-center items-center">
+  <div class="hex bg-black w-11 h-11 flex justify-center items-center">
+  <div class="hex bg-gold w-10 h-10 flex justify-center items-center pb-1">
+  <span class="text-white font-semibold text-center text-xl transition duration-300">${day}</span>
+  </div>
+  </div>
+  </div>
+  </div>
+  </button>
+</div>
+`;
 }
--- src/components/Calendar/CalendarMonthContainer.ts
+++ src/components/Calendar/CalendarMonthContainer.ts
@@ -1,7 +1,7 @@
 export default function CalendarMonthContainer(month: string): string {
   return `
   <!--* ${month.toUpperCase()} *-->
-  <div id="month-${month}" class="group ">
+  <div id="month-${month}" class="group">
   <h2
     class="text-gold2 opacity-50 font-semibold text-center mb-2 transition-opacity group-hover:opacity-80">
     ${month.slice(0, 3)}
