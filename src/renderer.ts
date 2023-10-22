84449f640f549d984d6f1a6a3070431ea8a68805
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-10-23 02:31:15 +0300

    v1.11.0

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,59 +1,20 @@
 import "./index.css";
 import TitleBar from "./components/TitleBar/TitleBar";
 import Sidebar from "./components/Sidebar/Sidebar";
-import Header from "./components/Header/Header";
-import Calendar from "./components/Calendar/Calendar";
 import TitleBarClock from "./components/TitleBar/TitleBarClock";
-import CalendarMonthContainer from "./components/Calendar/CalendarMonthContainer";
-import CalendarMonth from "./components/Calendar/CalendarMonth";
-import AdditionalOptionsMenu from "./components/AdditionalOptionsMenu/AdditionalOptionsMenu";
-// import AddNewHabitFunc from "./components/Sidebar/AddNewHabitFunc";
-import addRemoveActiveDay from "./components/Calendar/addRemoveActiveDay";
-import HabitBtnFunc from "./components/Sidebar/HabitBtnFunc";
 import SettingsBox from "./components/Settings/SettingsBox";
 import Home from "./components/Home/Home";
 const ROOT = document.querySelector("div#root");
 
 ROOT.append(TitleBar());
-ROOT.innerHTML += Sidebar();
-// ROOT.innerHTML += Header();
-// ROOT.innerHTML += Calendar();
-ROOT.append(SettingsBox()); ///
-ROOT.append(Home()); ///
-
-//**  RENDER CALENDER (One time only) **//
-const monthNames = [
-  ["January", 31],
-  ["February", 28],
-  ["March", 31],
-  ["April", 30],
-  ["May", 31],
-  ["June", 30],
-  ["July", 31],
-  ["August", 31],
-  ["September", 30],
-  ["October", 31],
-  ["November", 30],
-  ["December", 31],
-];
-// monthNames.forEach((month) => {
-//   document.querySelector("main").innerHTML += CalendarMonthContainer(
-//     month[0].toString()
-//   );
-//   CalendarMonth(month[0].toString(), +month[1]);
-// });
-//** **//
-
-// ROOT.innerHTML += AdditionalOptionsMenu();
+ROOT.append(Sidebar());
+ROOT.append(SettingsBox());
+ROOT.append(Home());
 
 //? FUNCTIONS ?//
 
 TitleBarClock();
 setInterval(TitleBarClock, 60000);
-
-// AddNewHabitFunc();
-// addRemoveActiveDay();
-// HabitBtnFunc();
 
 document.querySelector("#settings-open-btn").addEventListener("click", () => {
   document.querySelector("div#settings-box").classList.remove("hidden");
@@ -61,11 +22,3 @@ document.querySelector("#settings-open-btn").addEventListener("click", () => {
 document.querySelector("#settings-close-btn").addEventListener("click", () => {
   document.querySelector("div#settings-box").classList.add("hidden");
 });
-
-// document.querySelector("main").addEventListener("click", () => {
-// console.log(versions.chrome());
-// });
-
-// document.querySelector("main").addEventListener("click", () => {
-//   console.log(jsonFiles.createFolder("Testing"));
-// });
