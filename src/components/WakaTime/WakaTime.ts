38e7be2f4bc978c701697e2f56c6cae39b0bd685
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-10-28 17:46:57 +0200

    v1.14.6

--- src/components/WakaTime/WakaTime.ts
+++ src/components/WakaTime/WakaTime.ts
@@ -2,11 +2,12 @@ import HTML from "../HTML/HTML";
 
 export default function WakaTime() {
   const styles =
-    "h-[calc(100vh-31px)] w-[100%-3.5rem] ml-14 bg-neutral-950 border-t border-l border-neutral-400 p-4";
+    "h-[calc(100vh-31px)] w-[100%-3.5rem] ml-14 bg-neutral-950 border-t border-l border-neutral-400 p-4 overflow-auto";
 
   const MainContainer = HTML("main", styles, "WakaTime");
 
-  const styles2 = "bg-pink-900 flex justify-center py-4";
+  // --------
+  const styles2 = " flex justify-center py-4";
   const div1 = HTML("div", styles2);
   const img1 = HTML("img");
   img1.setAttribute(
@@ -15,15 +16,35 @@ export default function WakaTime() {
   );
   div1.append(img1);
 
-  // const div1 = HTML("div", "bg-red-500 w-44 h-44");
-  // const img2 = HTML("img", "w-10 mx-auto my-4");
-  // img.setAttribute(
-  //   "src",
-  //   "https://wakatime.com/share/@AlexEG/96b52650-2866-42cd-9ced-c922d8fdc7e8.svg"
-  // );
-  // div1.append(img2);
+  // --------
+  const styles3 = "grid grid-cols-4 gap-1 relative";
+  const grid = HTML("div", styles3);
+
+  // --------
+  const styles4 =
+    "hover:w-[1000px] hover:z-10 hover:absolute hover:top-1/2 hover:left-1/2 hover:-translate-x-1/2 hover:-translate-y-1/2";
+
+  const embedsURL = [
+    "https://wakatime.com/share/@AlexEG/96b52650-2866-42cd-9ced-c922d8fdc7e8.svg",
+    "https://wakatime.com/share/@AlexEG/a29a39e5-baad-4ae5-84a5-8189c050ac73.svg",
+    "https://wakatime.com/share/@AlexEG/8765dfb5-9a90-4133-a011-07b87e9b6ed8.svg",
+    "https://wakatime.com/share/@AlexEG/0ce04439-b9b3-492e-85f0-af15cde7d901.svg",
+    "https://wakatime.com/share/@AlexEG/e5c768c1-1627-4f4e-9c31-8b9d6e07b9d0.svg",
+    "https://wakatime.com/share/@AlexEG/f155aaeb-8225-4e92-a56c-4fca559f1ebd.svg",
+    "https://wakatime.com/share/@AlexEG/f36b0d33-be7a-493a-aa24-e1aad03ec444.svg",
+    "https://wakatime.com/share/@AlexEG/8c5d6e4e-2c83-4a6d-9106-4ac8d1714190.svg",
+  ];
+
+  for (let i = 0; i < embedsURL.length; i++) {
+    const figure = HTML("figure");
+    const embed = HTML("embed");
+    embed.setAttribute("src", embedsURL[i]);
+
+    figure.append(embed);
+    grid.append(figure);
+  }
 
   MainContainer.append(div1);
-  // MainContainer.append(div1);
+  MainContainer.append(grid);
   return MainContainer;
 }
