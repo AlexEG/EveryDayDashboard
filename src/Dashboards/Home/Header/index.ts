4eb2b51a209db893abb028f892a00586304234a3
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-11-22 20:09:01 +0200

    v2.12.2

--- src/Dashboards/Home/Header/index.ts
+++ src/Dashboards/Home/Header/index.ts
@@ -2,13 +2,17 @@ import HTML from "../../../components/HTML/HTML";
 import SelectMonthBtn from "../../../components/Settings/buttons/SelectMonthBtn";
 
 export default function Header() {
-  const styles =
-    "py-1 px-2 relative border border-emerald-500 flex justify-end";
+  const styles = "py-1 px-2 relative flex justify-end";
   const Header = HTML("header", styles);
-
+  const p = HTML(
+    "p",
+    "text-emerald-500 text-sm font-medium self-center mr-auto",
+    "",
+    "Check after the task is finished"
+  );
   function aa() {
     console.log("first");
   }
-  Header.append(SelectMonthBtn(aa));
+  Header.append(p, SelectMonthBtn(aa));
   return Header;
 }
