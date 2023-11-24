fa279f787366137afa13cd1d96af958532ef1978
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-11-24 22:55:15 +0200

    v2.14.1

--- src/Dashboards/CodeWars/Header.ts
+++ src/Dashboards/CodeWars/Header.ts
@@ -2,9 +2,11 @@ import HTML from "../../components/HTML/HTML";
 import RefreshBtn from "./RefreshBtn";
 import SelectMonthBtn from "../../components/Settings/buttons/SelectMonthBtn";
 import HonorScoreChart from "./HonorScoreChart";
+import Badge from "./Badge";
+import codewarsDATA from "./codewarsDATA";
 
 export default function Header() {
-  const styles = "flex justify-end gap-x-4";
+  const styles = "flex justify-between";
   const Header = HTML("div", styles);
 
   const img = HTML("img", "mr-auto", "", "", {
@@ -29,6 +31,37 @@ export default function Header() {
     );
   }
 
-  Header.append(img, SelectMonthBtn(changeViewedMonth), RefreshBtn());
+  // SelectMonthBtn & RefreshBtn wrraper
+  const wrraper1 = HTML("div", "flex gap-x-2");
+  wrraper1.append(SelectMonthBtn(changeViewedMonth), RefreshBtn());
+  // ----------------------------------
+  const wrraper2 = HTML("div", "flex gap-x-1");
+  // get DATA!
+  codewarsDATA().then((data) => {
+    const rank = data["data"]["Rank"];
+    const honor = data["data"]["Honor"];
+    const leaderboardPositionor = data["data"]["Leaderboard Position"];
+    // Header Badges
+    const Rank = Badge(`Rank: ${-rank} kyu`);
+    const Honor = Badge(`Honor: ${honor}`);
+    const LeaderboardPosition = Badge(
+      `Leaderboard Position:#${leaderboardPositionor.toLocaleString()}`
+    );
+    // 600_000 is not
+    const HonorPercentile = Badge(
+      `Top ${((leaderboardPositionor / 600_000) * 100).toFixed(3)}%`
+    );
+    const TotalCompletedKata = Badge("Completed Kata: 80");
+
+    wrraper2.append(
+      Rank,
+      Honor,
+      LeaderboardPosition,
+      HonorPercentile,
+      TotalCompletedKata
+    );
+  });
+  // ----------------------------------
+  Header.append(wrraper2, wrraper1);
   return Header;
 }
--- src/Dashboards/CodeWars/RefreshBtn.ts
+++ src/Dashboards/CodeWars/RefreshBtn.ts
@@ -3,7 +3,7 @@ import codewarsAPI_update from "./codewarsAPI_update";
 
 export default function RefreshBtn() {
   const styles =
-    "text-indigo-100 text-center font-medium px-3 py-px border-2 rounded-md border-indigo-600 hover:text-indigo-600 transition-colors duration-300 hover:border-indigo-100 hover:bg-indigo-100 active:opacity-90";
+    "text-indigo-100 text-center font-medium px-3 py-px border-2 border-indigo-600 hover:text-indigo-600 transition-colors duration-300 hover:border-indigo-100 hover:bg-indigo-100 active:opacity-90";
 
   const refreshBtn = HTML("button", styles, "", "Update JSON");
 
