4c4bc75f4ef89a931ebfa7cb85aa2ea162f55325
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-11-30 14:05:36 +0200

    v2.14.13

--- src/components/Sidebar/SectionBtn.ts
+++ src/components/Sidebar/SectionBtn.ts
@@ -15,6 +15,7 @@ export default function SectionBtn(sectionName: string, sectionHTML: any) {
   div.append(img);
 
   div.onclick = () => {
+    const PREF_LOG_START = Date.now();
     if (!(document.querySelector("main").getAttribute("id") == sectionName)) {
       console.log(
         ` %c OPEN  ${sectionName} `,
@@ -65,6 +66,16 @@ export default function SectionBtn(sectionName: string, sectionHTML: any) {
       );
       settingsBtn.dataset.isSettingsOpen = "false";
     }
+
+    const PREF_LOG_END = Date.now();
+    const PREF_LOG_CSS = [
+      "background:#000; color:#fff",
+      "background:#000; color:#0f0",
+    ];
+    console.log(
+      `%c Preformance / Time  %c${PREF_LOG_END - PREF_LOG_START}ms`,
+      ...PREF_LOG_CSS
+    );
   };
 
   return div;
--- src/renderer.ts
+++ src/renderer.ts
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar/Sidebar";
 import Settings from "./Settings";
 import Home from "./Dashboards/Home/Home";
 // import CodeWars from "./Dashboards/CodeWars";
+const PREF_LOG_START = Date.now();
 
 const ROOT = document.querySelector("div#root");
 ROOT.append(TitleBar());
@@ -12,3 +13,13 @@ ROOT.append(Settings());
 
 ROOT.append(Home());
 // ROOT.append(CodeWars());
+
+const PREF_LOG_END = Date.now();
+const PREF_LOG_CSS = [
+  "background:#000; color:#fff",
+  "background:#000; color:#0f0",
+];
+console.log(
+  `%c Preformance / Time  %c${PREF_LOG_END - PREF_LOG_START}ms`,
+  ...PREF_LOG_CSS
+);
