b3e31730c2b51bf661a4bb66930ef447296e4f70
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-12-03 06:04:42 +0200

    v2.15.0
    Rebuilding Habit Tracker

--- src/components/Sidebar/Sidebar.ts
+++ src/components/Sidebar/Sidebar.ts
@@ -4,6 +4,7 @@ import SettingsBtn from "./SettingsBtn";
 import SidebarrDATA from "../../Settings/SettingsPages/Sidebar/SidebarDATA";
 
 import Home from "../../Dashboards/Home/Home";
+import HabitTracker from "../../Dashboards/Habit Tracker";
 import GitHub from "../../Dashboards/GitHub/GitHub";
 import WakaTime from "../../Dashboards/WakaTime/WakaTime";
 import CodeWars from "../../Dashboards/CodeWars";
@@ -43,6 +44,8 @@ export default function Sidebar() {
         const func =
           svgName === "home"
             ? Home
+            : svgName === "habit-tracker"
+            ? HabitTracker
             : svgName === "github"
             ? GitHub
             : svgName === "wakatime"
