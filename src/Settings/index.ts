3623cb674e255463e85d7fc6a1232f1f84a2b735
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-12-17 04:43:34 +0200

    v2.20.3

--- src/Settings/index.ts
+++ src/Settings/index.ts
@@ -2,16 +2,16 @@ import HTML from "../components/HTML/HTML";
 import CloseSettingBtn from "./CloseSettingBtn";
 import NavSidebar from "./NavSidebar";
 // import Titlebar from "./SettingsPages/Titlebar";
-// import Sidebar from "./SettingsPages/Sidebar";
-import HabitTracker from "./SettingsPages/HabitTracker";
+import Sidebar from "./SettingsPages/Sidebar";
+// import HabitTracker from "./SettingsPages/HabitTracker";
 
 export default function Settings() {
   const styles =
-    "bg-neutral-900 fixed top-[72px] left-[97px] bottom-10 right-10 z-[999] flex";
+    "bg-neutral-900 fixed top-[72px] left-[97px] bottom-10 right-10 z-[999] flex invisible";
   // invisible
   const settingsContainer = HTML("section", styles, "settings");
   settingsContainer.dataset.isSettingsOpen = "false";
 
-  settingsContainer.append(CloseSettingBtn(), NavSidebar(), HabitTracker());
+  settingsContainer.append(CloseSettingBtn(), NavSidebar(), Sidebar());
   return settingsContainer;
 }
