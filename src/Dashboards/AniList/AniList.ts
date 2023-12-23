7f4d17eae819cd6242c6c437bfef76fd8af1cfc0
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-12-23 18:19:05 +0200

    v3.3.0
    start building the UI
    Header

--- src/Dashboards/AniList/AniList.ts
+++ src/Dashboards/AniList/AniList.ts
@@ -1,22 +1,12 @@
 import HTML from "../../components/HTML/HTML";
-import AniList_API_Favourites from "./API/AniList_API_Favourites";
-import AniList_API_UserInfo from "./API/AniList_API_UserInfo";
+import HomeBanner from "./Header/HomeBanner";
 
 export default function AniList() {
   const styles =
-    "h-[calc(100vh-31px)] w-[100%-3.5rem] ml-14 bg-neutral-950 border-t border-l border-neutral-400 p-4";
+    "h-[calc(100vh-31px)] w-[100%-3.5rem] ml-14 bg-black border-t border-l border-neutral-400 p-4";
 
   const MainContainer = HTML("main", styles, "anilist");
-  const h1 = HTML("h1", "text-center text-neutral-50 font-bold", "", "AniList");
 
-  // AniList_API_Favourites().then((data) => {
-  //   console.log("data", data);
-  // });
-
-  AniList_API_UserInfo().then((data) => {
-    console.log("User Info", data);
-  });
-
-  MainContainer.append(h1);
+  MainContainer.append(HomeBanner());
   return MainContainer;
 }
---
+++ src/Dashboards/AniList/Header/HomeBanner.ts
@@ -0,0 +1,67 @@
+import HTML from "../../../components/HTML/HTML";
+
+interface API_UserInfo {
+  data: {
+    User: {
+      about: string | null;
+      avatar: { large: string };
+      bannerImage: string;
+      name: string;
+    };
+  };
+}
+export default function HomeBanner() {
+  const styles =
+    "w-full h-72 border border-rose-600 relative rounded-lg overflow-hidden";
+  const header = HTML("header", styles);
+
+  // --------------- //
+  const userInfoJSON = new Promise((res, rej) => {
+    res(
+      JSON.parse(
+        window.DATA.getJSONFileData("dashboards/anilist/user-info-data")
+      )
+    );
+  });
+
+  userInfoJSON.then((data: API_UserInfo) => {
+    console.log("API UserInfoData: ", data);
+
+    const avatarURL = data.data.User.avatar.large;
+    const bannerURL = data.data.User.bannerImage;
+
+    const avatarImgFileName = String(
+      avatarURL.match(/(?<=avatar\/large\/).*/g)
+    );
+    const bannerImgFileName = String(bannerURL.match(/(?<=banner\/).*/g));
+
+    // console.log("avatarURL", avatarURL);
+    // console.log("bannerURL", bannerURL);
+    // console.log("bannerImgFileName", bannerImgFileName);
+    // console.log("avatarImgFileName", avatarImgFileName);
+
+    const bannerImg = HTML("img", "object-cover w-full h-full", "", "", {
+      src: `/DATA/dashboards/anilist/media/user/${bannerImgFileName}`,
+    });
+    const styles2 =
+      "w-full max-w-7xl h-20 border border-blue-600 absolute bottom-1 left-1/2 -translate-x-1/2 flex gap-x-2 pl-2";
+    const div = HTML("div", styles2);
+    const avatarImg = HTML(
+      "img",
+      "w-20 h-20 drop-shadow-[0_0_2px_rgb(0,0,0,0.5)]",
+      "",
+      "",
+      {
+        src: `/DATA/dashboards/anilist/media/user/${avatarImgFileName}`,
+      }
+    );
+    const styles3 =
+      "text-white font-extrabold text-lg text-center flex items-center border border-green-600 drop-shadow-[0_0_10px_rgb(0,0,0,1)]";
+    const userName = HTML("span", styles3, "", "AlexEG");
+
+    div.append(avatarImg, userName);
+    header.append(bannerImg, div);
+  });
+
+  return header;
+}
